a9c7fad691854536117f93aadcdf06cbe38ffdae
Cass11037 <mihaelissebastam750@gmail.com>
2025-03-12 01:23:15 +0300

    Changed details in Types.

--- src/main/kotlin/model/FuelType.kt
+++ src/main/kotlin/model/FuelType.kt
@@ -1,14 +1,9 @@
 package org.example.model
 
-enum class FuelType (val type: String) {
-    KEROSENE("kerosene"),
-    DIESEL("diesel"),
-    ALCOHOL("alcohol"),
-    MANPOWER("manpower"),
-    NUCLEAR("nuclear");
-    companion object {
-        fun fromString(type: String): FuelType? {
-            return FuelType.entries.find { it.type == type }
-        }
-    }
+enum class FuelType () {
+    KEROSENE,
+    DIESEL,
+    ALCOHOL,
+    MANPOWER,
+    NUCLEAR
 }
--- src/main/kotlin/model/VehicleType.kt
+++ src/main/kotlin/model/VehicleType.kt
@@ -1,13 +1,7 @@
 package org.example.model
 
-enum class VehicleType (val type: String) {
-    BOAT("boat"),
-    BICYCLE("bicycle"),
-    HOVERBOARD("hoverboard");
-    companion object {
-        // Функция для преобразования строки в VehicleType
-        fun fromString(type: String): VehicleType? {
-            return entries.find { it.type == type }
-        }
-    }
+enum class VehicleType () {
+    BOAT,
+    BICYCLE,
+    HOVERBOARD
 }
