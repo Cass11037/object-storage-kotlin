5e09c722b2fc69bddb5f5049a45a0c170ab2f9e9
Cass11037 <mihaelissebastam750@gmail.com>
2025-03-11 13:41:04 +0300

    RemoveAnyByEnginePower(): added class

--- src/main/kotlin/commands/RemoveAnyByCharacteristicCommand.kt
+++ src/main/kotlin/commands/RemoveAnyByCharacteristicCommand.kt
@@ -2,17 +2,19 @@ package org.example.commands
 
 import org.example.core.CollectionManager
 
-class RemoveAnyByCharacteristicCommand (
+abstract class RemoveAnyByCharacteristicCommand(
+    name: String,
+    description: String
 ) : Command(
-    name = "remove_any_by_engine_power",
-    description = "Remove an item by engine power."
+    name = name,
+    description = description
 ){
     override fun execute(args: List<String>, collectionManager: CollectionManager) {
         if (args.isEmpty() || args.size != 2) {
             println("Error: Args can not be empty.")
             return
         }
-        1val vehicle = collectionManager.findByCharacteristic(args[0], args[1])
+        val vehicle = collectionManager.findByCharacteristic(args[0], args[1])
         if(vehicle == null)  {
             println("No vehicle found with $args[0] = $args[1]")
             return
