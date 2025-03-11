8ed098e561714d9320a3da2706cdf03ba37be71b
Cass11037 <mihaelissebastam750@gmail.com>
2025-03-11 12:02:29 +0300

    AddIfMinCommand: added execute

---
+++ src/main/kotlin/commands/AddIfMinCommand.kt
@@ -0,0 +1,20 @@
+package org.example.commands
+
+import org.example.core.CollectionManager
+import org.example.core.VehicleReader
+
+class AddIfMinCommand (  private val reader: VehicleReader) : Command (
+    name = "add_if_min",
+    description = "Add a new item to the collection if its value is less than that of the smallest item in this collection."
+) {
+    override fun execute(args: List<String>, collectionManager: CollectionManager) {
+        val newVehicle = reader.readVehicle()
+        val minVehicle = collectionManager.getMin()
+        if(minVehicle == null || minVehicle > newVehicle) {
+            collectionManager.addVehicle(newVehicle)
+            println("Vehicle added with ID: ${newVehicle.id}")
+        } else {
+            println("New vehicle\'s value is not less then the value of the smallest item in that collection.")
+        }
+    }
+}
